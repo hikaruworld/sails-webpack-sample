ac6466960cdd4d2b42858f42d6776857d82d692e
hikaruworld <hikaruworld@gmail.com>
2015-01-01 12:55:25 +0900

    sails generate api user index

---
+++ api/controllers/UserController.js
@@ -0,0 +1,21 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  }
+};
+
